71156e0482a29b2a603617abb426b62f46edf195
Angélica D. Lorenz <167655164+AngelicaLorenz@users.noreply.github.com>
2026-05-21 18:24:24 -0300

    RF01 e RF02
    Definição do objeto do candidato e o array de vagas fictícias utilizando const.

---
+++ skillmatch.js
@@ -0,0 +1,37 @@
+// RF01 - Objeto representando o candidato
+const candidato = {
+    nome: "Angélica",
+    area: "Front-End",
+    habilidades: ["HTML", "CSS", "JavaScript", "Github", "Lógica de Programação", "Kanban"],
+    experienciaMeses: 8
+};
+
+// RF02 - Array com pelo menos 03 vagas fictícias
+const vagas = [
+    {
+        id: 1,
+        empresa: "Tech Systems",
+        cargo: "Desenvolvedor Front-End Júnior",
+        requisitos: ["HTML", "CSS", "JavaScript", "Github", "Lógica de Programação"],
+        salario: 2900,
+        modalidade: "Remoto"
+    },
+
+    {
+        id: 2,
+        empresa: "123 Code",
+        cargo: "Estágio Front-End",
+        requisitos: ["HTML", "CSS", "JavaScript"],
+        salario: 1700,
+        modalidade: "Híbrido"
+    },
+
+    {
+        id: 3,
+        empresa: "Web Tech",
+        cargo: "Programador Javascript",
+        requisitos: ["JavaScript", "NodeJS", "Github", "Kanban", "Lógica de Programação"],
+        salario: 3500,
+        modalidade: "Presencial"
+    }
+];
