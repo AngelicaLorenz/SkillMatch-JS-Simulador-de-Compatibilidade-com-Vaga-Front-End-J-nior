a514e6a92d77eadc4b3f1cd0a9b36e8c60d5f57e
Angélica D. Lorenz <167655164+AngelicaLorenz@users.noreply.github.com>
2026-05-22 10:01:18 -0300

    feat/classe, herança e this.

--- skillmatch.js
+++ skillmatch.js
@@ -14,7 +14,8 @@ const vagas = [
         cargo: "Desenvolvedor Front-End Júnior",
         requisitos: ["HTML", "CSS", "JavaScript", "Github", "Lógica de Programação"],
         salario: 2900,
-        modalidade: "Remoto"
+        modalidade: "Remoto",
+        nivel: "Júnior"
     },
 
     {
@@ -23,16 +24,45 @@ const vagas = [
         cargo: "Estágio Front-End",
         requisitos: ["HTML", "CSS", "JavaScript"],
         salario: 1700,
-        modalidade: "Híbrido"
+        modalidade: "Híbrido",
+        nivel: "Estágio"
     },
 
     {
         id: 3,
         empresa: "Web Tech",
-        cargo: "Programador Javascript",
+        cargo: "Programador Javascript Pleno",
         requisitos: ["JavaScript", "NodeJS", "Github", "Kanban", "Lógica de Programação"],
         salario: 3500,
-        modalidade: "Presencial"
+        modalidade: "Presencial",
+        nivel: "Pleno"
     }
 ];
 
+// RF09 - Classe Base Vaga
+class Vaga {
+    constructor(empresa, cargo, requisitos, salario, modalidade){
+        this.empresa = empresa;
+        this.cargo = cargo;
+        this.requisitos = requisitos;
+        this.salario = salario;
+        this.modalidade = modalidade;
+    }
+    
+    // RF11 - Demonstração do uso do 'this'
+    exibirResumo() {
+        return `${this.cargo} na empresa ${this.empresa}`;
+    }
+}
+
+// RF10 - Classe Especializada com Herança
+class VagaFrontEnd extends Vaga {
+    constructor(empresa, cargo, requisitos, salario, modalidade, nivel){
+        super(empresa, cargo, requisitos, salario, modalidade);
+        this.nivel = nivel;
+    }   
+    
+    exibirNivel() {
+    return `Nível da vaga: ${this.nivel}`;
+    }
+}
